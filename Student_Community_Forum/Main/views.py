469790d32fbb3c59f7fa6ec6fc4eed584f9e86a9
Juan Cervantes <jc.cervantes7@yahoo.com>
2025-03-09 00:38:18 -0600

    Implementing landing page

--- Student_Community_Forum/Main/views.py
+++ Student_Community_Forum/Main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
 # Create your views here.
 
-def index(response):
-    return HttpResponse("<h1>Hello, World!</h1>")
+def home(request):
+    return render(request, 'Main/home.html')
+
+
 
